c5e051eb04ee46f47aa5af06d6aba6dc850ab1ee
Kanikayudha Tantyo N <kanikayudha.17@students.amikom.ac.id>
2024-06-23 23:04:02 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,64 @@
+import React, { useState } from 'react'
+import { View, StyleSheet } from 'react-native'
+import CustomButton from './src/components/customButton'
+import CustomTextInput from './src/components/customTextInput'
+import Home from './src/screens/home'
+import AddNote from './src/screens/addNote'
+import EditNote from './src/screens/editNote'
+
+const CurrentPageWidget = ({
+  currentPage,
+  noteList,
+  setCurrentPage,
+  addNote,
+}) => {
+  switch (currentPage) {
+    case 'home':
+      return <Home noteList={noteList} setCurrentPage={setCurrentPage} />
+    case 'add':
+      // Berikan function "addNote" ke component "AddNote"
+      return <AddNote setCurrentPage={setCurrentPage} addNote={addNote} />
+    case 'edit':
+      return <EditNote />
+    default:
+      return <Home />
+  }
+}
+
+const App = () => {
+  const [currentPage, setCurrentPage] = useState('home')
+  const [noteList, setNoteList] = useState([
+    {
+      id: 1,
+      title: 'Note pertama',
+      desc:
+        'Lorem Ipsum is simply dummy text of the printing and typesetting industry',
+    },
+  ])
+
+  const addNote = (title, desc) => {
+    const id =
+      noteList.length > 0 ? noteList[noteList.length - 1].id + 1 : 1
+  
+    setNoteList([
+      ...noteList,
+      {
+        id,
+        title: title,
+        desc: desc,
+      },
+    ])
+  }
+
+  return (
+    <CurrentPageWidget
+      currentPage={currentPage}
+      noteList={noteList}
+      setCurrentPage={setCurrentPage}
+      // Berikan function addNote sebagai prop
+      addNote={addNote}
+    />
+  );
+}
+
+export default App
