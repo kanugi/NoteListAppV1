39e722eaa976d7eba0e26e1ce7fe6dbdfce730d3
Kanikayudha Tantyo N <kanikayudha.17@students.amikom.ac.id>
2024-06-23 23:19:53 +0700

    add note feature

--- src/screens/addNote.js
+++ src/screens/addNote.js
@@ -3,7 +3,7 @@ import { View, StyleSheet, Text } from 'react-native'
 import CustomButton from '../components/customButton'
 import CustomTextInput from '../components/customTextInput'
 
-const AddNote = ({ setCurrentPage }) => {
+const AddNote = ({ setCurrentPage, addNote }) => {
   const [title, setTitle] = useState('')
   const [desc, setDesc] = useState('')
 
@@ -32,6 +32,10 @@ const AddNote = ({ setCurrentPage }) => {
           color="#fff"
           text="Simpan"
           width="100%"
+          onPress={() => {
+            addNote(title, desc)
+            setCurrentPage('home')
+          }}
         />
       </View>
       <View style={styles.spacerTop}>
